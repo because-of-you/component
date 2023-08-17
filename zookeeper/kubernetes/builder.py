cb37f80ad6c90b97bad275740b339f5af513baf0
wfy-belief <mail@acitrus.cn>
2023-08-17 23:52:37 +0800

    fix: 修复节点名称问题 --part2

--- zookeeper/kubernetes/builder.py
+++ zookeeper/kubernetes/builder.py
@@ -36,7 +36,7 @@ def zoo_conf_config(identity: str, node_name: str):
     with open(os.path.join(path, "zoo.cfg"), "a+") as file:
         for i in range((int(identity) + 1)):
             node_name = node_name.split("-")[0]
-            file.write(f"server.{i}={node_name}:2888:3888;2181" + os.linesep)
+            file.write(f"server.{i}={node_name}-{i}:2888:3888;2181" + os.linesep)
         file.close()
 
 
