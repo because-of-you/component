dfb4bc3a6df18f32c9a0426391fcff9aa727891f
wfy-belief <mail@acitrus.cn>
2023-09-05 17:16:59 +0800

    add: 配置文件自动生成broker id

--- kafka/kubernetes/builder.py
+++ kafka/kubernetes/builder.py
@@ -7,42 +7,32 @@ from typing import Sequence
 
 logging.basicConfig(level=logging.INFO, format=logging.BASIC_FORMAT)
 BASE_DIR = "/opt/component"
-SERVICE_NAME = "zookeeper"
-CONF_DIR = "conf"
+SERVICE_NAME = "kafka"
+CONF_DIR = "config"
 
 path = os.path.join(BASE_DIR, SERVICE_NAME, CONF_DIR)
 
 
-def my_id_file(identity: str):
-    DATA_DIR = "/tmp/zookeeper"
-    with open(os.path.join(path, "zoo.cfg"), "r") as file:
+def server_properties(identity: str):
+    DATA_DIR = "/opt/component/kafka/data"
+    with open(os.path.join(path, "server.properties"), "r") as file:
         for line in file.readlines():
             words = line.split("=")
             if len(words) != 2:
                 continue
-            if words[0] == "dataDir":
+            if words[0] == "log.dirs":
                 DATA_DIR = words[-1].strip()
-            if words[0] == "dataLogDir":
-                data_log_dir = words[-1].strip()
-                if not os.path.exists(data_log_dir):
-                    os.makedirs(data_log_dir)
 
         file.close()
 
     if not os.path.exists(DATA_DIR):
         os.makedirs(DATA_DIR)
 
-    with open(os.path.join(DATA_DIR, "myid"), "w+") as file:
-        file.write(identity + os.linesep)
+    with open(os.path.join(DATA_DIR, "server.properties"), "a+") as file:
+        file.write("broker.id=" + identity + os.linesep)
         file.close()
 
 
-def zoo_conf_config(identity: str, node_name: str):
-    with open(os.path.join(path, "zoo.cfg"), "a+") as file:
-        for i in range(max(int(identity) + 1, 2)):
-            node_name = node_name.split("-")[0]
-            file.write(f"server.{i}={node_name}-{i}.zookeeper-service:2888:3888;2181" + os.linesep)
-        file.close()
 
 
 def main():
@@ -58,12 +48,10 @@ def main():
     identity = splits[-1]
     logging.info(f"解析的id是 {identity}")
 
-    shutil.copy2(os.path.join(path, "zoo_sample.cfg"), os.path.join(path, "zoo.cfg"))
-    # zoo.cfg
-    zoo_conf_config(identity, uname.nodename)
+    shutil.copy2(os.path.join(path, "server_sample.properties"), os.path.join(path, "server.properties"))
 
-    # myid
-    my_id_file(identity)
+    # server.properties
+    server_properties(identity)
 
 
 if __name__ == '__main__':
