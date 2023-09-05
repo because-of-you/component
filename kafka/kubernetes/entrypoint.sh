1d1305f81d4330129e998d09ea2440463b949d71
wfy-belief <mail@acitrus.cn>
2023-09-05 16:34:09 +0800

    change: 增加kafka配置

--- kafka/kubernetes/entrypoint.sh
+++ kafka/kubernetes/entrypoint.sh
@@ -1,15 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# 获取唯一id
-identifier=${HOSTNAME##*-}
+./builder
 
-if ! [[ "$identifier" =~ ^[0-9]+$ ]]; then
-    echo "不能使用表达式: identifier=\${HOSTNAME##*-} 提取唯一数字id HOSTNAME=$HOSTNAME identifier=$identifier | 示例HOSTNAME=xxx-1"
-    tail
-fi
+# 保证网络加入正常 防止启动过快
+sleep 2s
 
-# 构造文件
-echo "broker.id=$identifier" >> "$KAFKA_HOME"/config/server.properties
-
-# 启动
-kafka-server-start.sh "$KAFKA_HOME"/config/server.properties
+exec "$@"
