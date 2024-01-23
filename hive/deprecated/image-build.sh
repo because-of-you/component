37fe9a5494bc956c2c1250ccf1c654da993606d1
wangfy02 <wangfy02@mingyuanyun.com>
2024-01-23 10:50:25 +0800

    feat: 废弃hive旧版本内容

--- hive/deprecated/image-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#docker build -f DockerFile --force-rm -t wfybelief/component:hive .
-docker buildx build -f DockerFile --platform linux/arm64,linux/amd64 -t wfybelief/component:hive . --push
