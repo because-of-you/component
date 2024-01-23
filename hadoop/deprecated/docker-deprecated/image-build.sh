3e10bdb4f9db248fa4e8c9e676a2ab32fcbaadc3
wfy-belief <mail@acitrus.cn>
2024-01-23 10:50:06 +0800

    feat: 废弃hadoop旧版本仓库内容

--- hadoop/deprecated/docker-deprecated/image-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#docker build -f DockerFile --force-rm -t wfybelief/component:hadoop .
-docker buildx build -f DockerFile --platform linux/arm64,linux/amd64 -t wfybelief/component:hadoop . --push
